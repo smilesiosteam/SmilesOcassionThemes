ba533d8d189900dee67f66bdaca363f7047c246b
Abdul Rehman Amjad <abdulrehman.amjad@systemsltd.com>
2023-11-09 13:55:01 +0500

    - ThemeId initializer dependency injection - Code refactoring - Removed unwanted code

--- Sources/SmilesOcassionThemes/OcassionThemeMVVM-RC/VC/OcassionThemesVC.swift
+++ Sources/SmilesOcassionThemes/OcassionThemeMVVM-RC/VC/OcassionThemesVC.swift
@@ -19,38 +19,44 @@ import AnalyticsSmiles
 import SmilesBanners
 import SmilesPersonalizationEvent
 
-
-
 public class OcassionThemesVC: UIViewController {
     
+    // MARK: - OUTLETS -
     @IBOutlet weak var navBarView: UIView!
     @IBOutlet weak var topBarHeight: NSLayoutConstraint!
     @IBOutlet weak var navTitle: UILabel!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var tableViewTopConstraint: NSLayoutConstraint!
-    public  var input: PassthroughSubject<OccasionThemesViewModel.Input, Never> = .init()
+    
+    // MARK: - PROPERTIES -
+    public var input: PassthroughSubject<OccasionThemesViewModel.Input, Never> = .init()
     private var cancellables = Set<AnyCancellable>()
     private lazy var viewModel: OccasionThemesViewModel = {
         return OccasionThemesViewModel()
     }()
     var topBrands: [GetTopBrandsResponseModel.BrandDO]?
     var dataSource: SectionedTableViewDataSource?
-    var sections =  [TableSectionData<OccasionThemesSectionIdentifier>]()
+    var sections = [TableSectionData<OccasionThemesSectionIdentifier>]()
     //[OccasionThemesSectionData]()
     var occasionThemesSectionsData: GetSectionsResponseModel?
-    public var themeid: Int = 1
-    private let isGuestUser: Bool = false
-    public var delegate:SmilesOccasionThemesHomeDelegate?
-   
-    var hasTopNotch: Bool {
-        if #available(iOS 11.0, tvOS 11.0, *) {
-            return UIApplication.shared.delegate?.window??.safeAreaInsets.top ?? 0 > 20
-        }
-        return false
-    }
+    private var themeId: Int = 1
+    private var delegate:SmilesOccasionThemesHomeDelegate?
     var isHeaderExpanding = false
     var topBannerObject: ThemeResponseModel?
-    public init(delegate: SmilesOccasionThemesHomeDelegate?) {
+    
+    // MARK: - ACTIONS -
+    @IBAction func backButton(sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    @IBAction func didPressedSearchbutton(sender: UIButton) {
+        if let delegate = delegate {
+            delegate.navigateToGlobalSearch()
+        }
+    }
+    
+    // MARK: - INITIALIZER -
+    public init(delegate: SmilesOccasionThemesHomeDelegate?, themeId: Int) {
+        self.themeId = themeId
         self.delegate = delegate
         super.init(nibName: "OcassionThemesVC", bundle: Bundle.module)
     }
@@ -67,11 +73,11 @@ public class OcassionThemesVC: UIViewController {
         getSections()
     }
     
-    
     public override func viewWillAppear(_ animated: Bool) {
         self.topBarHeight.constant = 88
         self.navigationController?.navigationBar.isHidden = true
     }
+    
     // MARK: - Helping Functions
     func setupTableView() {
         self.tableView.sectionFooterHeight = .leastNormalMagnitude
@@ -85,7 +91,6 @@ public class OcassionThemesVC: UIViewController {
         let customizable: CellRegisterable? = OccasionThemesCellRegistration()
         customizable?.register(for: self.tableView)
         self.tableView.backgroundColor = .white
-        
     }
     
     //MARK: Navigation Bar Setup
@@ -110,6 +115,7 @@ public class OcassionThemesVC: UIViewController {
         
 //        self.homeAPICalls()
     }
+    
     func adjustTopHeader(_ scrollView: UIScrollView) {
         guard isHeaderExpanding == false else {return}
         if let tableView = scrollView as? UITableView {
@@ -132,25 +138,15 @@ public class OcassionThemesVC: UIViewController {
             }
         }
     }
-    // MARK: - IBActions
-    @IBAction func backButton(sender: UIButton) {
-        self.navigationController?.popViewController(animated: true)
-    }
-    @IBAction func didPressedSearchbutton(sender: UIButton) {
-        if let delegate = delegate {
-            delegate.navigateToGlobalSearch()
-        }
-    }
+   
 }
 
+// MARK: - SERVER CALLS -
 extension OcassionThemesVC {
-    // MARK: - Get Sections Api Calls
+    
     private func getSections() {
-        self.input.send(.getSections(themeId: self.themeid))
+        self.input.send(.getSections(themeId: self.themeId))
     }
-    
-    
-    // MARK: - HomeApi Calls
     
     private func homeAPICalls() {
         
@@ -160,52 +156,46 @@ extension OcassionThemesVC {
                 guard let sectionIdentifier = element.sectionIdentifier, !sectionIdentifier.isEmpty else {
                     return
                 }
-//                if let section = OccasionThemesSectionIdentifier(rawValue: sectionIdentifier), section != .topPlaceholder {
-//                                    sections.append(TableSectionData(index: index, identifier: section))
-//                }
                 
                 switch OccasionThemesSectionIdentifier(rawValue: sectionIdentifier) {
-                    
                 case .topPlaceholder:
-                    self.input.send(.getThemesDetail(themeId: self.themeid))
+                    self.input.send(.getThemesDetail(themeId: self.themeId))
 //                    if let bannerIndex = getSectionIndex(for: .topPlaceholder) {
 //                        guard let bannerSectionData = self.occasionThemesSectionsData?.sectionDetails?[bannerIndex] else {return}
 //                       
 //                      //  self.configureUpgardeBanner(with: bannerSectionData, index: bannerIndex)
 //                    }
                 case .themeItemCategories:
-                    
                     if let response = ThemeCategoriesResponse.fromModuleFile() {
                         self.dataSource?.dataSources?[index] = TableViewDataSource.make(forItemCategories: [response], data:"#FFFFFF", isDummy:true, onClick: nil)
                         
                     }
-                    self.input.send(.getThemeCategories(themeId: self.themeid))
-                    break
-                case .stories:
+                    self.input.send(.getThemeCategories(themeId: self.themeId))
                     
+                case .stories:
                     if let stories =  Stories.fromModuleFile() {
                         self.dataSource?.dataSources?[index] = TableViewDataSource.make(forStories: stories, data:"#FFFFFF", isDummy:true, onClick: nil)
                     }
+                    self.input.send(.getStories(themeid: self.themeId, pageNo: 1))
                     
-                    self.input.send(.getStories(themeid: self.themeid, pageNo: 1))
                 case .topCollections:
-                    
                     if let response = GetCollectionsResponseModel.fromFile() {
                         self.dataSource?.dataSources?[index] = TableViewDataSource.make(forCollections: response, data:"#FFFFFF", isDummy: true, completion: nil)
                     }
-                    self.input.send(.getCollections(themeId: self.themeid, menuItemType: nil))
-                case .topBrands:
+                    self.input.send(.getCollections(themeId: self.themeId, menuItemType: nil))
                     
+                case .topBrands:
                     if let response = GetTopBrandsResponseModel.fromFile() {
                         self.dataSource?.dataSources?[index] = TableViewDataSource.make(forBrands: response, data:"#FFFFFF", isDummy: true, topBrandsType: .foodOrder, completion: nil)
                     }
-                    self.input.send(.getTopBrands(themeId: self.themeid, menuItemType: nil))
+                    self.input.send(.getTopBrands(themeId: self.themeId, menuItemType: nil))
                     
                 default: break
                 }
             }
         }
-        self.tableView.reloadData()
+        configureDataSource()
+        
     }
     
     // MARK: - Section Data
@@ -220,14 +210,13 @@ extension OcassionThemesVC {
             self.configureDataSource()
         }
         homeAPICalls()
+        
     }
     
 }
 
-
+// MARK: - VIEWMODEL CINDING -
 extension OcassionThemesVC {
-    
-    // MARK: - Section Data
     
     func bind(to viewModel: OccasionThemesViewModel) {
         input = PassthroughSubject<OccasionThemesViewModel.Input, Never>()
@@ -240,45 +229,52 @@ extension OcassionThemesVC {
                     
                 case .fetchSectionsDidFail(error: let error):
                     debugPrint(error.localizedDescription)
-                    
+
                 case .fetchStoriesDidSucceed(let stories):
                     self?.configureStoriesData(with: stories)
                     
                 case .fetchStoriesDidFail(let error):
                     debugPrint(error.localizedDescription)
                     self?.configureHideSection(for: .stories, dataSource: Stories.self)
+                    
                 case .fetchTopBrandsDidSucceed(response: let response):
                     debugPrint(response)
                     self?.configureTopBrandsData(with: response)
+                    
                 case .fetchTopBrandsDidFail(error: _):
                     self?.configureHideSection(for: .topBrands, dataSource: GetTopBrandsResponseModel.self)
+                    
                 case .fetchCollectionsDidSucceed(response: let response):
                     self?.configureCollectionsData(with: response)
+                    
                 case .fetchCollectionDidFail(error: let error):
                     debugPrint(error.localizedDescription)
                     self?.configureHideSection(for: .topCollections, dataSource: GetCollectionsResponseModel.self)
+                    
                 case .fetchThemeCategoriesDidSucceed(response: let response):
                     debugPrint(response)
+                    
                 case .fetchThemeCategoriesDidFail(error: let error):
                     debugPrint(error.localizedDescription)
+                    
                 case.fetchThemeDetailDidSucceed(response: let response):
                     self?.topBannerObject = response
                     self?.configureDataSource()
-                case.fetchThemeDetailDidFail(error: _):
-                    break
+                    
+                case.fetchThemeDetailDidFail(error: let error):
+                    debugPrint(error.localizedDescription)
                 }
             }.store(in: &cancellables)
     }
     
 }
+
+// MARK: - SECTIONS CONFIGURATIONS -
 extension OcassionThemesVC {
-    
-    // MARK: - SECTIONS CONFIGURATIONS -
     
     fileprivate func configureCollectionsData(with collectionsResponse: GetCollectionsResponseModel) {
         
         if let collections = collectionsResponse.collections, !collections.isEmpty {
-            
             if let topCollectionsIndex = getSectionIndex(for: .topCollections) {
                 self.dataSource?.dataSources?[topCollectionsIndex] = TableViewDataSource.make(forCollections: collectionsResponse, data: self.occasionThemesSectionsData?.sectionDetails?[topCollectionsIndex].backgroundColor ?? "#FFFFFF", completion: { [weak self] data in
                     
@@ -292,11 +288,12 @@ extension OcassionThemesVC {
         } else {
             self.configureHideSection(for: .topCollections, dataSource: GetCollectionsResponseModel.self)
         }
+        
     }
+    
     fileprivate func configureStoriesData(with storiesResponse: Stories) {
         
         if let stories = storiesResponse.stories, !stories.isEmpty {
-            
             if let storiesIndex = getSectionIndex(for: .stories) {
                 self.dataSource?.dataSources?[storiesIndex] = TableViewDataSource.make(forStories: storiesResponse, data: self.occasionThemesSectionsData?.sectionDetails?[storiesIndex].backgroundColor ?? "#FFFFFF", onClick: { [weak self] story in
                     if var stories = ((self?.dataSource?.dataSources?[safe: storiesIndex] as? TableViewDataSource<Stories>)?.models)?.first {
@@ -320,12 +317,13 @@ extension OcassionThemesVC {
             print("Hide stories section is being called")
             self.configureHideSection(for: .stories, dataSource: Stories.self)
         }
+        
     }
     
     fileprivate func configureTopBrandsData(with topBrandsResponse: GetTopBrandsResponseModel) {
+        
         self.topBrands = topBrandsResponse.brands
         if let brands = topBrandsResponse.brands, !brands.isEmpty {
-            
             if let topBrandsIndex = getSectionIndex(for: .topBrands) {
                 self.dataSource?.dataSources?[topBrandsIndex] = TableViewDataSource.make(forBrands: topBrandsResponse, data: self.occasionThemesSectionsData?.sectionDetails?[topBrandsIndex].backgroundColor ?? "#FFFFFF", topBrandsType: .foodOrder, completion: { [weak self] data in
 //                    let analyticsSmiles = AnalyticsSmiles(service: FirebaseAnalyticsService())
@@ -342,6 +340,7 @@ extension OcassionThemesVC {
             print("Hide top brand is being called")
             self.configureHideSection(for: .topBrands, dataSource: GetTopBrandsResponseModel.self)
         }
+        
     }
     
     fileprivate func configureHideSection<T>(for section: OccasionThemesSectionIdentifier, dataSource: T.Type) {
@@ -349,9 +348,9 @@ extension OcassionThemesVC {
         if let index = getSectionIndex(for: section) {
             (self.dataSource?.dataSources?[index] as? TableViewDataSource<T>)?.models = []
             (self.dataSource?.dataSources?[index] as? TableViewDataSource<T>)?.isDummy = false
-            
             self.configureDataSource()
         }
+        
     }
 }
 
--- Sources/SmilesOcassionThemes/Protocols/OcassionThemesVCMainDelegate.swift
+++ Sources/SmilesOcassionThemes/Protocols/OcassionThemesVCMainDelegate.swift
@@ -6,17 +6,10 @@
 //
 
 import Foundation
-import SmilesOffers
-import SmilesStoriesManager
-public enum OccasionThemesHomeNavigationType {
-    case payment, withTextPromo, withQRPromo, freeTicket
-}
 
 public protocol SmilesOccasionThemesHomeDelegate {
     
     func handleDeepLinkRedirection(redirectionUrl: String)
     func navigateToGlobalSearch()
-    func navigateToLocation()
-    func navigateToRewardPoint(personalizationEventSource: String?)
 
 }
